59e4001c8699d0741dcea4ff8529844425a58ac8
bantai-education-design <ban.tai3educationdesign@gmail.com>
2026-08-24 18:55:37 +0900

    Bust cache for typed university selector

--- tools/university-database/tokyo/photo-register/university-selection-sync-fix.js
+++ tools/university-database/tokyo/photo-register/university-selection-sync-fix.js
@@ -64,13 +64,13 @@ queueMicrotask(()=>{
   if(!document.querySelector('link[data-simple-register-flow]')){
     const link=document.createElement('link');
     link.rel='stylesheet';
-    link.href='simple-register-flow.css?v=20260824-1822';
+    link.href='simple-register-flow.css?v=20260824-1858';
     link.dataset.simpleRegisterFlow='true';
     document.head.appendChild(link);
   }
   if(!document.querySelector('script[data-simple-register-flow]')){
     const simple=document.createElement('script');
-    simple.src='simple-register-flow.js?v=20260824-1822';
+    simple.src='simple-register-flow.js?v=20260824-1858';
     simple.defer=true;
     simple.dataset.simpleRegisterFlow='true';
     document.body.appendChild(simple);
